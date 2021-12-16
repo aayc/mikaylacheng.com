c9f0657fb1ead2e61eed9ec06b03836ed9012729
Aaron Chan <aaronchan@Aarons-MacBook-Pro.local>
2021-12-15 22:02:16 -0700

    Install tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,21 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React from 'react'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Mikayla Cheng</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
+        <h1 className="text-3xl">mikayla cheng</h1>
+        <br />
+        <p className="bg-green-300 description">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-
-      <Footer />
     </div>
   )
 }
